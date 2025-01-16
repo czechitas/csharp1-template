191697541d3e73ce5e0ba0e64b5817b4a1e86658
Martin Černil <m.cernil@gmail.com>
2025-01-16 23:12:44 +0100

    Fix build

--- src/Lekce01.Ukol/Program.cs
+++ src/Lekce01.Ukol/Program.cs
@@ -1,6 +1,7 @@
-﻿/*
+﻿Console.WriteLine("Vítej na kurzu Czechitas C#1");
+/*
 Prvni povinný úkol
 Zkuste si proklikat hru na https://studio.code.org/hoc/1
 Jako důkaz splnění odevzdejte v tomto úkolu odkaz na certifikát, který obdržíte po splnění 20. úkolu.
 Pokud se na něčem zaseknete, rozhodně se obraťte na vaše kouče pro radu ;)
-    */
+*/
