9e5a9182105a18eb3c5885472849d8d0f29211e9
Martin Černil <martin.cernil@thermofisher.com>
2025-03-12 08:48:08 +0100

    Po lekci 8

---
+++ src/Lekce08.Vyklad.RytirADrak/Program.cs
@@ -0,0 +1,121 @@
+﻿internal class Program
+{
+    /*
+    Vytvoř třídu Rytíř, který bude mít vlastnosti: jméno, počet životů a sílu
+    Ve třídě rytíř vytvoř metodu UtocNaDraka, která na konzoli vypíše "Útočím na draka!"
+    Ve třídě rytíř vytvoř metodu PredstavSe, která vypíše informace o rytíři.
+    */
+
+    public class Rytir
+    {
+        public Rytir(string jmeno, int pocetZivotu)
+        {
+            Jmeno = jmeno;
+            PocetZivotu = pocetZivotu;
+        }
+
+        public Rytir(string jmeno)
+        {
+            Jmeno = jmeno;
+            PocetZivotu = 5;
+        }
+
+        private string Jmeno;
+        private int PocetZivotu;
+
+        public void PredstavSe()
+        {
+            Console.WriteLine($"Ja jsem rytir {Jmeno}, mam {PocetZivotu} zivotu.");
+        }
+
+        public void UtocimNaDraka(Drak drak)
+        {
+            Console.WriteLine($"Utocim na draka {drak.ZiskejJmeno("wouldyoukindly")}");
+        }
+    }
+    /*
+    Vytvoř třídu Drak, který bude mít vlastnosti: jméno, počet hlav a silu
+    Ve třídě drak vytvoř metodu PredstavSe, která vypíše informace o drakovi.
+    Ve třídě drak vytvoř metodu ZasazenRytirem, která drakovi sníží počet hlav o 1 a vypíše, "Kde mám hlavu!" a pokud už jsou všechny hlavy pryč, napíše "Jsem poražen ..."
+    */
+    public class Drak
+    {
+        public Drak(string jmeno, int pocetHlav)
+        {
+            Jmeno = jmeno;
+            if (pocetHlav < 1)
+            {
+                PocetHlav = 1;
+            }
+            else
+            {
+                PocetHlav = pocetHlav;
+            }
+        }
+        private string Jmeno;
+        private int PocetHlav;
+
+        public string ZiskejJmeno(string heslo)
+        {
+            if (heslo == "wouldyoukindly")
+            {
+                return Jmeno;
+            }
+            return "Unknown";
+        }
+        public void PredstavSe()
+        {
+            Console.WriteLine($"Jsem Drak {Jmeno}, mam {PocetHlav} hlav.");
+        }
+
+        /*Metoda ZasazenRytirem, která drakovi sníží počet hlav o 1 a vypíše, "Kde mám hlavu!"
+         a pokud už jsou všechny hlavy pryč, napíše "Jsem poražen ..."*/
+        public void ZasazenRytirem()
+        {
+            if (PocetHlav > 0)
+            {
+                Console.WriteLine("Kde mam hlavu?");
+                PocetHlav--; //ekvivalentni zapis: PocetHlav = PocetHlav - 1;
+            }
+            else
+            {
+                Console.WriteLine("Jsem porazen...");
+            }
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        // //1. Naplneni vlastnosti
+        // Rytir bonifac = new Rytir(); //vytvoreni instance tridy Rytir
+        // // Zmena vlastnasti instance "bonifac"
+        // bonifac.PredstavSe();
+
+        // bonifac.Jmeno = "Bonifac";
+        // bonifac.PocetZivotu = 5;
+        // bonifac.Sila = 3;
+        // bonifac.PredstavSe();
+
+        // //2. Object initializer
+        // Rytir ferdinand = new Rytir
+        // {
+        //     Jmeno = "Ferdinand",
+        //     PocetZivotu = 4,
+        //     Sila = 8,
+        // };
+
+        //3. Pouziti konstruktoru
+        Rytir konstantin = new Rytir("Konstantin", 10);
+        konstantin.PredstavSe();
+
+        Drak trupik = new Drak("Trupik", 0);
+        trupik.PredstavSe();
+
+        konstantin.UtocimNaDraka(trupik);
+        trupik.ZasazenRytirem();
+        trupik.ZasazenRytirem();
+        trupik.ZasazenRytirem();
+        trupik.ZasazenRytirem();
+        trupik.ZasazenRytirem();
+    }
+}
---
+++ src/Lekce08.Vyklad.StatickeMetodyAVlastnosti/Program.cs
@@ -0,0 +1,93 @@
+﻿internal class Program
+{
+
+
+    public static int NactiCeleCisloOdUzivatele()
+    {
+        Console.WriteLine("Zadej cele cislo:");
+        string vstupUzivatele = Console.ReadLine();
+        int prevedenyVstup;
+        bool povedloSePrevest = int.TryParse(vstupUzivatele, out prevedenyVstup);
+
+        while (!povedloSePrevest)
+        {
+            Console.WriteLine("Zadej cele cislo:");
+            vstupUzivatele = Console.ReadLine();
+            povedloSePrevest = int.TryParse(vstupUzivatele, out prevedenyVstup);
+        }
+
+        return prevedenyVstup;
+    }
+    private static void Main(string[] args)
+    {
+        int vysledek = MatematickeOperace.Secti(5, 4);
+        Console.WriteLine(vysledek);
+
+        int prvniCislo = NactiCeleCisloOdUzivatele();
+
+        // Rytir bonifac = new Rytir();
+        // bonifac.UtocNaDraka();
+
+        //Nelze vytvaret instance statickych trid
+        //MatematickeOperace matematickeOperace = new MatematickeOperace();
+        vysledek = MatematickeOperace.NaDruhou(4);
+        Console.WriteLine(vysledek);
+
+        Circle kruh = new Circle(5);
+        double obvodMehoKruhu = kruh.VratObvod();
+        Console.WriteLine(obvodMehoKruhu);
+
+        double vypocitanyObvodZPolomeru = Circle.VypocitejObvodZPolomeru(8);
+        Console.WriteLine(vypocitanyObvodZPolomeru);
+
+        Console.WriteLine("Toto je volani staticke metody Console.WriteLine");
+
+
+        Circle kruh1 = new Circle(5);
+        Circle kruh2 = new Circle(6);
+        Circle kruh3 = new Circle(7);
+        Circle kruh4 = new Circle(8);
+
+    }
+
+    public static class MatematickeOperace
+    {
+        public static int Secti(int prvniCislo, int druheCislo)
+        {
+            return prvniCislo + druheCislo;
+        }
+
+        public static int NaDruhou(int cislo)
+        {
+            return cislo * cislo;
+        }
+    }
+
+    public class Circle
+    {
+        public Circle(double polomer)
+        {
+            Polomer = polomer;
+            PocetVytvorenychKruhu++;
+        }
+        private double Polomer;
+        public static int PocetVytvorenychKruhu;
+
+        //Vrati Obvod vytvorene instance objektu
+        public double VratObvod()
+        {
+            return 2 * Math.PI * Polomer;
+        }
+        // Staticka metoda, k dispozici bez instance objektu
+        public static double VypocitejObvodZPolomeru(int jakykolivPolomer)
+        {
+            return 2 * Math.PI * jakykolivPolomer;
+        }
+    }
+
+    class JablkaGala
+    {
+        public static string JmenoOdrudy = "Gala";
+        public static string Barva = "Červená";
+    }
+}
